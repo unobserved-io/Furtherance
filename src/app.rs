420f9560b81870358685c4e391b1af39f552cb51
Ricky Kresslein <rk@lakoliu.com>
2024-08-17 15:43:45 +0200

    Blank out task_input on timer stop

--- src/app.rs
+++ src/app.rs
@@ -169,6 +169,7 @@ impl Application for Furtherance {
                             rate,
                         })
                         .expect("Couldn't write task to database.");
+                        self.task_input = "".to_string();
                         self.task_history = get_task_history();
                     }
                     self.timer_text = "0:00:00".to_string();
