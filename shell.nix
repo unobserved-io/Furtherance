f5c32c8e66a0543779d19f7a2d33ea118253fbd1
Ricky Kresslein <ricky@unobserved.io>
2024-09-10 08:52:22 +0200

    Add nix shell script

---
+++ shell.nix
@@ -0,0 +1,23 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell rec {
+  buildInputs = with pkgs; [
+    cargo
+    rustc
+    fontconfig
+    pkg-config
+    cmake
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+    xorg.libXScrnSaver
+    wayland
+    wayland-protocols
+    libxkbcommon
+    vulkan-loader
+  ];
+
+  LD_LIBRARY_PATH = builtins.foldl' (a: b: "${a}:${b}/lib") "${pkgs.vulkan-loader}/lib" buildInputs;
+
+}
