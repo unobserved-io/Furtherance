f70d25a8cf6ad47b684313069bf6933b5019bf34
Ricky Kresslein <ricky@unobserved.io>
2024-09-21 08:22:16 +0200

    Load REQUIRED_FONT_BYTES

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,7 @@ fn main() -> iced::Result {
     iced::application(Furtherance::title, Furtherance::update, Furtherance::view)
         .subscription(Furtherance::subscription)
         .theme(Furtherance::theme)
+        .font(iced_fonts::REQUIRED_FONT_BYTES)
         .font(iced_fonts::BOOTSTRAP_FONT_BYTES)
         .window_size((1024.0, 600.0))
         .run()
