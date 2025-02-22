99f0c2e6c392767598ef304d9f48c372e3411f1e
Ricky Kresslein <ricky@unobserved.io>
2025-02-22 16:59:18 +0100

    Wayland detection bug - fixes #168 by @amjadorfali

--- src/helpers/idle.rs
+++ src/helpers/idle.rs
@@ -63,7 +63,7 @@ pub fn get_idle_time() -> u64 {
 
 #[cfg(target_os = "linux")]
 fn is_wayland() -> bool {
-    if let Ok(_) = env::var("XDG_SESSION_TYPE").map(|v| v == "wayland") {
+    if let Ok(true) = env::var("XDG_SESSION_TYPE").map(|v| v == "wayland") {
         return true;
     } else if let Ok(_) = env::var("WAYLAND_DISPLAY") {
         return Path::new(&format!("/run/user/{}/wayland-0", get_current_uid())).exists();
