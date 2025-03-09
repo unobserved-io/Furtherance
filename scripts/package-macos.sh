a418d5c48f05c926aedb668be8549d123934d52b
Ricky Kresslein <ricky@unobserved.io>
2025-03-09 06:36:30 +0100

    Add delay before creating dmg

--- scripts/package-macos.sh
+++ scripts/package-macos.sh
@@ -10,5 +10,6 @@ DMG_DIR="$RELEASE_DIR/macos"
 # package dmg
 echo "Packing disk image..."
 ln -sf /Applications "$DMG_DIR/Applications"
+sleep 5
 hdiutil create "$DMG_DIR/$DMG_NAME" -volname "Furtherance" -fs HFS+ -srcfolder "$APP_DIR" -ov -format UDZO
 echo "Packed '$APP_NAME' in '$APP_DIR'"
